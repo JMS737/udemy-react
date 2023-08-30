10cbabd6f61102b7fc5c3ab368f8ebfcf79ee377
Jeremy Sapsed <dev@jsapsed.co.uk>
2023-08-30 21:29:44 +0100

    Format fixes

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 
 export default function Note() {
-    return (
-      <div className="note">
-        <h1>Note Title</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio ut
-          enim blandit volutpat maecenas volutpat blandit. Ultricies mi quis
-          hendrerit dolor.
-        </p>
-      </div>
-    );
+  return (
+    <div className="note">
+      <h1>Note Title</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Odio ut enim blandit
+        volutpat maecenas volutpat blandit. Ultricies mi quis hendrerit dolor.
+      </p>
+    </div>
+  );
 }
