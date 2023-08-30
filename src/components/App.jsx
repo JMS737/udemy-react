907972c44cbbe8510508ae70cbd15c7bf4935c02
Jeremy Sapsed <dev@jsapsed.co.uk>
2023-08-30 21:26:30 +0100

    Initial react app layout for note app

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "./Footer";
+import Note from "./Note";
+import Header from "./Header";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
